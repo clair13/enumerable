0f609ccb1693915529705e5302e80f9c5392363c
clair13 <theclairvoyant1313@yahoo.gr>
2019-06-30 20:05:54 +0300

    Implementmy_map my_count my_inject methods

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -34,5 +34,34 @@ module Enumerable
     true
   end
 
+  def my_count
+    if block_given?
+      arr = self.my_select { |i| return i if yield(x) }
+      arr.size
+    else
+      self.size
+    end
+  end
 
+  def my_map(proc = nil )
+    map_arr = []
+    if proc
+      self.my_each { |i| map_arr << proc.call(i) }
+    elsif block_given?
+      self.my_each { |i| map_arr << yield(i) }
+    end
+    map_arr
+  end
+
+  def my_inject(initial = nil)
+    initial ? result = initial : result = self[0]
+    for i in self[1...self.length] do
+      result = yield(result, i)
+    end
+    result
+  end 
+  
+  def multiply_els(array)
+    array.my_inject { |result, i| result * i}
+  end
 end
